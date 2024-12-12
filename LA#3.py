0f4ad0440e6ce381af11ab6f6335e64e62c36f9a
MarkRullan <164309766+MarkRullan@users.noreply.github.com>
2024-12-12 10:47:37 -0800

    Add files via upload

---
+++ LA#3.py
@@ -0,0 +1,13 @@
+#L.A. #
+#Mark J. Rullan
+
+class Character:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+        
+    def describe(self):
+        print(f"{self.name} is a {self.role} hero.")
+    
+hero = Character("Xborg", "Fighter")
+hero.describe()
