c54f3c39464f4e5658984ba13ae4290aa88adc5d
MarkRullan <164309766+MarkRullan@users.noreply.github.com>
2024-12-14 00:26:01 -0800

    LA#26.py

---
+++ LA#26main.py
@@ -0,0 +1,11 @@
+import turtles as Tt
+
+Leonardo = Tt.Leonardo ("Leonardo", "Blue")
+Michaelangelo = Tt.MichaelAngelo ("MichaelAngelo", "Yellow")
+Donatello = Tt.Donatello ("Donatello", "Purple")
+Raphael = Tt.Raphael ("Raphael", "Red")
+
+print(Leonardo.alias)
+print(Michaelangelo.alias)
+print(Donatello.alias)
+print(Raphael.alias)
