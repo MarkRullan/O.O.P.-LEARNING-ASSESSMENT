90d503ad3cccbd8c448b39e0d380a5e6fa4b403d
MarkRullan <164309766+MarkRullan@users.noreply.github.com>
2024-12-21 01:37:27 -0800

    LA#27.py

--- LA/27/LA#27.py
+++ LA/27/LA#27.py
@@ -1,57 +1,11 @@
-#L.A. #27
-#Mark J. Rullan
+import turtles as Tt
 
-from abc import ABC, abstractmethod
-class NinjaTurtle(ABC):
-    @property
-    @abstractmethod
-    def alias(self):
-        pass
-    
-class Leonardo(NinjaTurtle):
-    def  __init__(self, real_name, alias):
-        self.real_name = real_name
-        self.__alias = alias
-    
-    @property
-    def alias(self):
-        return f"{self.__alias}"
+Leonardo = Tt.Leonardo ("Leornardo", "Blue")
+Michaelangelo = Tt.Michaelangelo ("Michaelangelo", "Yellow")
+Donataelo = Tt.Donatello ("Donatello", "Purple")
+Raphael = Tt.Raphael ("Raphael", "Red")
 
-class Michelangelo(NinjaTurtle):
-    def  __init__(self, real_name, alias):
-        self.real_name = real_name
-        self.__alias = alias
-    
-    @property
-    def alias(self):
-        return f"{self.__alias}"
-
-class Donatello(NinjaTurtle):
-    def  __init__(self, real_name, alias):
-        self.real_name = real_name
-        self.__alias = alias
-    
-    @property
-    def alias(self):
-        return f"{self.__alias}"
-
-class Raphael(NinjaTurtle):
-    def  __init__(self, real_name, alias):
-        self.real_name = real_name
-        self.__alias = alias
-    
-    @property
-    def alias(self):
-        return f"{self.__alias}"
-
-if __name__ =="__main__":
-    
-    leonardo = Leonardo("Leonardo", "blue")
-    michalagelo = Michelangelo("Michelangelo", "orange")
-    donatello = Donatello("Donatello", "violet")
-    raphael = Raphael("Raphael", "red")
-
-    print(leonardo.alias)
-    print(michalagelo.alias)
-    print(donatello.alias)
-    print(raphael.alias)
+print(Leonardo.alias)
+print(Michaelangelo.alias)
+print(Donataelo.alias)
+print(Raphael.alias)
